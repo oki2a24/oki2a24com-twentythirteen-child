7898b18cd1f382d2dfdb5a027917141212e46d45
oki2a24 <oki2a24@gmail.com>
2018-06-20 11:50:38 +0900

    追加しようとして断念したのヘッダー画像用コードを削除する

--- functions.php
+++ functions.php
@@ -1,35 +1,5 @@
 <?php
 /**
- * 親テーマ Twenty Thirteen のセットアップに追加する処理を定義します。
- */
-function oki2a24_twentythirteen_child_setup() {
-        register_default_headers( array(
-                'PC292698' => array(
-                        'url' => '%2$s/images/headers/PC292698.JPG',
-                        'thumbnail_url' => '%2$s/images/headers/PC292698-thumbnail.JPG',
-                        /* translators: header image description */
-                        'description' => __( 'PC292698', 'twentyeleven' )
-                ),
-                'PC292705' => array(
-                        'url' => '%2$s/images/headers/PC292705.JPG',
-                        'thumbnail_url' => '%2$s/images/headers/PC292705-thumbnail.JPG',
-                        /* translators: header image description */
-                        'description' => __( 'PC292705', 'twentyeleven' )
-                ),
-                'PC292714' => array(
-                        'url' => '%2$s/images/headers/PC292714.JPG',
-                        'thumbnail_url' => '%2$s/images/headers/PC292714-thumbnail.JPG',
-                        /* translators: header image description */
-                        'description' => __( 'PC292714', 'twentyeleven' )
-                )
-        ) );            
-}                       
-/**
- * 親テーマ Twenty Thirteen のセットアップに処理を追加します。
- */
-//add_action( 'after_setup_theme', 'oki2a24_twentythirteen_child_setup' );
-
-/**
  * more タグで URL 末端に付く #more-xxxx を削除します。
  */
 function remove_more_link_scroll( $link ) {
